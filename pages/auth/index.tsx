d9cc832994e0a742a6b7f601d372ea2a506640ed
AlexC-ux <eunv2004@gmail.com>
2023-01-15 02:00:14 +0300

    prisma + reg

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -4,9 +4,9 @@ import axios from "axios";
 import { useRouter } from "next/router"
 import React, { Dispatch, SetStateAction, useContext, useState } from "react";
 import { useIntl } from "react-intl";
+import { GlobalContext } from "../../components/contextes/globalcontext";
 import Error from "../../components/interfaces/error";
 import { IUser } from "../../components/interfaces/user";
-import { GlobalContext } from "../../contextes/globalcontext";
 
 export default function SignIn() {
     const intl = useIntl();
@@ -15,7 +15,7 @@ export default function SignIn() {
 
     const [error, setError]: [any, Dispatch<SetStateAction<any>>] = useState({ text: undefined });
 
-    function AuthorizeByCreds() {
+    function authorizeByCreds() {
         axios.post("/api/auth/checkcreds", {
             username: "0",
             password: "123"
@@ -47,6 +47,15 @@ export default function SignIn() {
                         sx={{
                             display: !!error.text ? "block" : "none"
                         }}>{error.text}</Alert>
+                    <Typography
+                        variant="h5"
+                        component={"div"}
+                        align="center"
+                        sx={{
+                            pb:3
+                        }}>
+                        {intl.formatMessage({ id: "AUTH.signin" })}
+                    </Typography>
                     <Grid container spacing={2}>
                         <Grid item xs={12} md={6}>
                             <Typography
@@ -72,13 +81,16 @@ export default function SignIn() {
                         <Grid item xs={12} md={5}>
                             <Button type="submit"
                                 variant="outlined"
-                                onClick={AuthorizeByCreds}
+                                onClick={authorizeByCreds}
+                                color="secondary"
                                 fullWidth={true}>{intl.formatMessage({ id: "AUTH.signin" })}</Button>
                         </Grid>
                         <Grid item xs={0} md={7} />
                         <Grid item xs={12} md={5}>
                             <Button
                                 variant="outlined"
+                                color="secondary"
+                                onClick={() => { router.push(intl.locale + "/auth/newProfile") }}
                                 fullWidth={true}>{intl.formatMessage({ id: "AUTH.register" })}</Button>
                         </Grid>
                     </Grid>
